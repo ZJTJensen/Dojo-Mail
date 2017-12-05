002dd87a00d7894800f290d5f96f89435b3e5360
Zachary Jensen <ZJTJensen@yahoo.com>
2017-12-05 13:13:07 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  emails=[
+    {email: 'bill@gates.com', importance: true, subject: 'New Windows', content: 'Windows XI will launch in year 2100'},
+    {email: 'ada@lovelace.com', importance: true, subject: 'Programming', content: 'Enchantree of Numbers'},
+    {email: 'john@carmac.com', importance: false, subject: 'Updated Algo', content: 'New algo for shadow volumes'},
+    {email: 'gabe@newell.com', importance: false, subject: 'HL3', content: 'Just kiding'}
+  ]
 }
